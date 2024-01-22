216697655f8973727acf379d8849cdc1fa2f7272
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-22 23:49:14 +0530

    Update GetCountryInfoFromAzureOpenAI.py

--- utils/GetCountryInfoFromAzureOpenAI.py
+++ utils/GetCountryInfoFromAzureOpenAI.py
@@ -1,20 +1,28 @@
 import json
-import openai
+from openai import OpenAI
 import os
 from utils.env_config import get_config_value
 
 
 class GetCountryInfoFromAzureOpenAI:
     def __init__(self):
-        openai.api_type = "azure"
-        openai.api_base = get_config_value('OPENAI_API_BASE')
-        openai.api_version = get_config_value('OPENAI_API_VERSION')
-        openai.api_key = os.getenv("OPENAI_API_KEY")
+        # openai.api_type = "azure"
+        # openai.api_base = get_config_value('OPENAI_API_BASE')
+        # openai.api_version = get_config_value('OPENAI_API_VERSION')
+        # openai.api_key = os.getenv("OPENAI_API_KEY")
+        client = OpenAI(
+            api_version=get_config_value('OPENAI_API_VERSION'),
+            azure_deployment=get_config_value(
+                'COMPLETIONS_MODEL_DEPLOYMENT_NAME'),
+            azure_endpoint=get_config_value('OPENAI_API_BASE'),
+            api_key=os.getenv("OPENAI_API_KEY"),
+        )
 
     def get_country_info(self, country_name):
-        input_prompt = f"Please give me the country_name, capital_state, national_bird, country_population for {country_name} in flat JSON object. country_population should be in BIGINT without separators"
+        input_prompt = f"Please give me the country_name, capital_state, national_bird, country_population for {
+            country_name} in flat JSON object. country_population should be in BIGINT without separators"
 
-        response = openai.Completion.create(
+        response = self.client.Completion.create(
             engine=get_config_value('COMPLETIONS_MODEL_DEPLOYMENT_NAME'),
             prompt=input_prompt,
             temperature=1,
